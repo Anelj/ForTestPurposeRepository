2a0696494219fea0c0a326d4f1ea9caf6c5d5987
Anelj <Anelj@users.noreply.github.com>
2020-03-15 21:34:59 +0300

    Second commit

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+/**
+ * Created by penpa on 15.03.2020.
+ */
+public class NewClass {
+    private int b;
+
+    public NewClass(int b) {
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
